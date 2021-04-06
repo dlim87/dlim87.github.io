2e581a2d6c87d75d61c7e09705ea0f282360db05
Daniel Lim <dlim87@gmail.com>
2021-04-06 11:35:54 -0700

    replaced create-react-app page with placeholders

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import NavBar from './components/NavBar.jsx'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <NavBar />
       </header>
+      <body className="App-body">
+        <div>Coming Soon</div>
+      </body>
     </div>
   );
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+class NavBar extends React.Component {
+    render(){
+        return <span className="navbar">This is the navbar</span>
+    }
+}
+
+export default NavBar
